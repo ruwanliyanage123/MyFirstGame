42d8f067221e9ea5246a410e9a24dbdc6f17ce00
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-04-01 23:39:03 +0530

    make changes in Player.cs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Player : MonoBehaviour{
+    
+    public float moveSpeed = 600f;
+
+    float movement = 0f;
+
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate(){
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
     }
 }
